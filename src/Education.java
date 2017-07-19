c0634d08fa49a915b683ebffbb2af5895abb1da2
Matthew Menna <mattmenna@gmail.com>
2017-07-19 12:55:07 -0400

    Initial Commit

--- src/Education.java
+++ src/Education.java
@@ -1,2 +1,79 @@
 public class Education {
+    private String school;
+    private String major;
+    private String startDate;
+    private String endDate;
+    private String description;
+    private String city;
+    private String state;
+
+    public Education(String school, String major, String startDate, String endDate, String description, String city, String state) {
+        this.school = school;
+        this.major = major;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Education() {
+
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
 }
--- src/Portfolio.java
+++ src/Portfolio.java
@@ -6,6 +6,10 @@ public class Portfolio {
         Experience DPS = new Experience("Dearborn Public Schools", "Math and Science Teacher", "9/2016", "6/2017","7th Grade", "Dearborn", "Michigan");
         Experience AdvancedTechAcademy = new Experience("Advanced Technology Academy", "Math and Science Teacher", "9/2012", "6/2016","9th and 10th Grade", "Dearborn", "Michigan");
         Experience RR = new Experience("River Rouge High School", "Math and Science Teacher", "9/2016", "6/2017","7th Grade", "Dearborn", "Michigan");
+        Skills skills = new Skills();
+        Education MSU = new Education("Michigan State University", "BS in Astrophysics", "", "Graduated 2007", "", "East Lansing", "Michigan");
+        Education UofMDearborn = new Education("University of Michigan - Dearborn", "MA in Education", "", "Graduated 2009", "", "Dearborn", "Michigan");
+        Education GrandCircus = new Education("Grand Circus Detroit", "Java Developer Bootcamp", "06/2017", "08/2017", "", "Detroit", "Michigan");
 
     }
 }
